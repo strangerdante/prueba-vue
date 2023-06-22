4058513a4dc2d2e444e6d4aca6f295b3b1564314
Jamel <jpimientabe@gmail.com>
2023-06-21 23:38:23 -0500

    iconos agregados

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,20 @@ import Breadcrumb from "./components/Breadcrumb.vue";
     >
       <div class="m-4 flex text-white">
         <p>Best selling</p>
-        <p class="ml-auto text-gray-500">This week</p>
+        <p class="ml-auto text-gray-400">This week</p>
+        <!-- Icono flecha abajo -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1.2em"
+          viewBox="0 0 320 512"
+          fill="currentColor"
+          class="ml-2 text-gray-400"
+        >
+          <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+          <path
+            d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z"
+          />
+        </svg>
       </div>
       <div class="m-3 mx-6 flex rounded-lg bg-tarjetas p-1 px-3 text-white">
         <p>Tuesday</p>
@@ -45,14 +58,31 @@ import Breadcrumb from "./components/Breadcrumb.vue";
       <GraficoLinea2 />
     </div>
     <div
-      class="flex h-36 justify-center rounded-lg bg-gray-700 sm:col-start-1 sm:row-start-4"
+      class="h-36 flex-col justify-center rounded-lg bg-gray-700 p-4 sm:col-start-1 sm:row-start-4"
     >
-      <GraficoDona />
+      <div class="flex">
+        <p class="font-bold text-white">$456,623</p>
+        <p class="ml-auto text-gray-400">Monthly</p>
+        <!-- Icono flecha abajo -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1.2em"
+          viewBox="0 0 320 512"
+          fill="currentColor"
+          class="ml-2 text-gray-400"
+        >
+          <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+          <path
+            d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z"
+          />
+        </svg>
+      </div>
+      <GraficoLinea class="p-2" />
     </div>
     <div
       class="flex h-36 justify-center rounded-lg bg-gray-700 sm:col-start-2 sm:row-start-4"
     >
-      <GraficoLinea />
+      <GraficoDona />
     </div>
     <div
       class="flex h-52 justify-center rounded-lg bg-gradient-to-r from-teal-400 to-teal-700 sm:col-span-2 sm:col-start-3 sm:row-start-1"
@@ -68,8 +98,4 @@ import Breadcrumb from "./components/Breadcrumb.vue";
   </div>
 </template>
 
-<style>
-.grafico-container {
-  max-width: 150px;
-}
-</style>
+<style></style>
--- src/components/GraficoProgreso.vue
+++ src/components/GraficoProgreso.vue
@@ -3,7 +3,20 @@
     <div class="flex-col">
       <div class="flex">
         <p class="font-bold text-white">456k Pcs</p>
-        <p class="ml-auto text-white">Daily</p>
+        <p class="ml-auto text-gray-400">Daily</p>
+        <!-- Icono flecha abajo -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1.2em"
+          viewBox="0 0 320 512"
+          fill="currentColor"
+          class="ml-2 text-gray-400"
+        >
+          <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+          <path
+            d="M182.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128z"
+          />
+        </svg>
       </div>
 
       <p class="text-gray-500">Ticket Sold</p>
